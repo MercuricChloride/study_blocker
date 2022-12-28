e2ba04c7b8b25a97cfcd34f6fb2f12037b912c83
mercuricchloride <mercuryprotocol@protonmail.com>
2022-12-28 16:41:05 -0500

    better ui

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 mod blocking;
 
+use std::time::{Duration, SystemTime};
+
 use blocking::{block_domains, unblock_domains};
 
 use eframe::egui;
@@ -18,8 +20,8 @@ fn main() {
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
 struct StudyBlocker {
     domains: String,
-    length_of_study: u32,
-    start_time: u32,
+    length_of_study: u64,
+    start_time: SystemTime,
     blocking: bool, // we are going to use this to determine if we are blocking or not, so we can not copy a blocked hosts file
 }
 
@@ -28,7 +30,7 @@ impl Default for StudyBlocker {
         Self {
             domains: String::new(),
             length_of_study: 0,
-            start_time: 0,
+            start_time: SystemTime::now(),
             blocking: false,
         }
     }
@@ -54,7 +56,7 @@ impl eframe::App for StudyBlocker {
         eframe::set_value(storage, eframe::APP_KEY, self);
     }
 
-    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+    fn update(&mut self, ctx: &egui::Context, _: &mut eframe::Frame) {
         let Self {
             domains,
             length_of_study,
@@ -62,34 +64,53 @@ impl eframe::App for StudyBlocker {
             blocking,
         } = self;
 
-        let split_domains: Vec<String> = domains.split("\n").map(|item| item.to_owned()).collect();
+        let time_elapsed = SystemTime::now()
+            .duration_since(*start_time)
+            .unwrap()
+            .as_secs();
+
+        ctx.request_repaint_after(Duration::new(0, 100_000_000)); // 100ms
+
+        // if we are blocking and the time elapsed is greater than the length of study, unblock
+        if *blocking && *length_of_study != 0 && time_elapsed > *length_of_study {
+            unblock_domains();
+            *blocking = false;
+        }
 
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("Study Blocker");
 
             ui.separator();
 
-            // domain input
-            ui.label("Domains to Block");
-            ui.text_edit_multiline(domains);
+            // Length of study slider & domain input field
+            // only show if we are not blocking
+            if !*blocking {
+                // length of study input
+                ui.label("Duration of Study");
+                ui.add(egui::Slider::new(length_of_study, 0..=24).text("Minutes"));
+
+                // domain input
+                ui.label("Domains to Block");
+                ui.text_edit_multiline(domains);
+            } else {
+                // otherwise show the time left
+                let time_left = *length_of_study - time_elapsed;
+                ui.label(format!("Time Left: {} minutes", time_left));
+            }
 
             ui.separator();
 
-            ui.horizontal(|ui| {
-                if ui.button("Block Domains").clicked() {
-                    block_domains(domains.to_owned(), blocking);
-                    *blocking = true;
-                }
-
+            if *blocking {
                 if ui.button("Unblock Domains").clicked() {
                     unblock_domains();
                     *blocking = false;
                 }
-            });
-
-            // DEBUG: Displaying the split domains
-            for domain in split_domains {
-                ui.label(domain);
+            } else {
+                if ui.button("Block Domains").clicked() {
+                    block_domains(domains.to_owned(), blocking);
+                    *start_time = SystemTime::now();
+                    *blocking = true;
+                }
             }
         });
     }
